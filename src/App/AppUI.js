09d609ad5783ca43f3127cb2c73def3fa90ac818
Edwin Upegui <edwinupegui@gmail.com>
2022-02-23 14:48:31 -0500

    progreso clase 15

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,48 +1,44 @@
 import React from 'react';
+import { TodoContext } from '../TodoContext';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
 
-function AppUI( {
-  totalTodos,
-    completedTodos,
-    searchValue,
-    setSearchValue,
-    searchedTodos,
-    completeTodos,
-    deleteTodos
+function AppUI() {
 
-}) {
-  
   return (
     <React.Fragment>
-      <TodoCounter
-
-        total={totalTodos}
-        completed={completedTodos}
-
-      />
-      <TodoSearch
-
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-
-      />
+      <TodoCounter />
+      <TodoSearch />
 
 
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodos(todo.text)}
-            onDelete={() => deleteTodos(todo.text)}
-          />
-        ))}
-      </TodoList>
+      <TodoContext.Consumer>
+        {({ error,
+          loading,
+          searchedTodos,
+          completeTodos,
+          deleteTodos 
+        }) => (
+          <TodoList>
+            {error && <p>error</p>}
+            {loading && <p>cargando</p>}
+            {(!loading && !searchedTodos.length) && <p>Crear tu primer to do</p>}
+
+
+            {searchedTodos.map(todo => (
+              <TodoItem
+                key={todo.text}
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodos(todo.text)}
+                onDelete={() => deleteTodos(todo.text)}
+              />
+            ))}
+          </TodoList>
+        )};
+      </TodoContext.Consumer>
 
       <CreateTodoButton />
 
